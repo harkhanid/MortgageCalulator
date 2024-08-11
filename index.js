06838d7a3afb3d6b6ab30fbc203e2130c92d1085
Dharmik <harkhani.d@northeastern.edu>
2024-08-11 10:02:58 -0400

    Completed project

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ form.addEventListener("submit", (event) => {
   const rate = form.elements["rate"].value;
   const type = form.elements["type"].value;
   let validForm = true;
+
+  // amount validation
   if (!basicValidation("amount", amount)) {
     validForm = false;
   }
 
+  //term validation
   if (
     (!basicValidation("term", term) && 0 > Number(term)) ||
     Number(term) > 31
@@ -18,6 +21,7 @@ form.addEventListener("submit", (event) => {
     validForm = false;
   }
 
+  //rate validation
   if (
     (!basicValidation("rate", rate) && 0 > Number(rate)) ||
     Number(rate) > 100
@@ -26,12 +30,36 @@ form.addEventListener("submit", (event) => {
     validForm = false;
   }
 
-  if (!basicValidation("type")) {
+  //type validation
+  if (!basicValidation("type", type)) {
     validForm = false;
   }
   if (validForm == true) {
-    calculateAmount(Number(amount), Number(term), Number(rate), type);
+    calculateAmount(
+      getNumberFromCSN(amount),
+      getNumberFromCSN(term),
+      getNumberFromCSN(rate),
+      type
+    );
   }
+});
+
+document.getElementById("amount").addEventListener("input", function (e) {
+  let value = e.target.value;
+  const convertedNumber = convertNumber(value);
+  e.target.value = convertedNumber;
+});
+
+document.getElementById("term").addEventListener("input", function (e) {
+  let value = e.target.value;
+  value = value.replace(/[^0-9.]/g, "");
+  e.target.value = value;
+});
+
+document.getElementById("rate").addEventListener("input", function (e) {
+  let value = e.target.value;
+  value = value.replace(/[^0-9.]/g, "");
+  e.target.value = value;
 });
 
 const calculateAmount = (amount, term, rate, type) => {
@@ -51,38 +79,32 @@ const calculateAmount = (amount, term, rate, type) => {
     monthlypayment = totalPayment / 12 / term;
   }
 
-  document.getElementById("monthly-result").innerHTML =
-    monthlypayment.toFixed(2);
-  document.getElementById("total-pending").innerHTML = totalPayment.toFixed(2);
+  document.getElementById("monthly-result").innerHTML = convertNumber(
+    monthlypayment.toFixed(2)
+  );
+  document.getElementById("total-pending").innerHTML = convertNumber(
+    totalPayment.toFixed(2)
+  );
   document.getElementById("after-result").classList.remove("hide");
   document.getElementById("before-result").classList.add("hide");
 };
 
 const basicValidation = (id, input) => {
-  if (id == "type" && input === "") {
+  if (input === "") {
     displayError(id, "this field is required");
-  }
-  if (input !== "") {
-    if (isNaN(input)) {
-      displayError(id, "This field must be a number");
-      return false;
-    }
-  } else {
-    displayError(id, "This field is required");
     return false;
   }
-
-  if (input !== "") {
-    if (isNaN(input)) {
-      displayError(id, "This field must be a number");
-      return false;
-    }
-  } else {
-    displayError(id, "This field is required");
-    return false;
-  }
-
   return true;
+};
+
+const convertNumber = (value) => {
+  value = value.replace(/[^0-9.]/g, "");
+
+  const parts = value.split(".");
+
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  return parts.join(".");
 };
 
 const focusFn = (id) => {
@@ -96,6 +118,10 @@ const focusFn = (id) => {
 const clearFn = () => {
   document.getElementById("after-result").classList.add("hide");
   document.getElementById("before-result").classList.remove("hide");
+};
+
+const getNumberFromCSN = (string) => {
+  return Number(string.replace(/,/g, ""), 10);
 };
 
 const displayError = (id, message) => {
